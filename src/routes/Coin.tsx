14aa650868964942b10eb8497c9231db273ed15e
zomggang <gangeun25@gmail.com>
2024-11-04 23:45:11 +0900

    making still cart

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -163,7 +163,7 @@ function Coin() {
               <Price />
             </Route>
             <Route path={`/${coinId}/chart`}>
-              <Chart />
+              <Chart coinId={coinId} />
             </Route>
           </Switch>
         </>
